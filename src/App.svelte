afea20c82de14298dd653b6ed2cbe9f2345a43e8
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-10-27 16:42:10 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -13,9 +13,7 @@
     <style>
             body{
                 background-color: #f6f7f9;
-                height: 1700px;
-                margin: 0px;
-                margin-top:-40px
+              
              }
  
     </style>
